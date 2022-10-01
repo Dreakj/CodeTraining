48a3bc0825fe44acc4ff7d328c4ad275c75cbb41
Dreakj <469982670@qq.com>
2022-10-01 13:44:59 +0800

    2022-8-27

--- service/service_statistics/src/main/java/com/atguigu/staservice/controller/StatisticsDailyController.java
+++ service/service_statistics/src/main/java/com/atguigu/staservice/controller/StatisticsDailyController.java
@@ -6,6 +6,8 @@ import com.atguigu.staservice.service.StatisticsDailyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 /**
  * <p>
  * 网站统计日数据 前端控制器
@@ -24,6 +26,11 @@ public class StatisticsDailyController {
     public R createStatisticsByDate(@PathVariable String day) {
         dailyService.createStatisticsByDay(day);
         return R.ok();
+    }
+
+    public R showChart(@PathVariable String begin, @PathVariable String end, @PathVariable String type) {
+        Map<String, Object> map = dailyService.getChartData(begin, end, type);
+        return R.ok().data(map);
     }
 }
 
--- service/service_statistics/src/main/java/com/atguigu/staservice/service/StatisticsDailyService.java
+++ service/service_statistics/src/main/java/com/atguigu/staservice/service/StatisticsDailyService.java
@@ -3,6 +3,8 @@ package com.atguigu.staservice.service;
 import com.atguigu.staservice.entity.StatisticsDaily;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Map;
+
 /**
  * <p>
  * 网站统计日数据 服务类
@@ -14,4 +16,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface StatisticsDailyService extends IService<StatisticsDaily> {
     public void createStatisticsByDay(String day);
 
+    Map<String, Object> getChartData(String begin, String end, String type);
 }

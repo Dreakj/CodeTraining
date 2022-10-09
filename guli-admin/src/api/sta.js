1486815e6105378923bcb26def2bd0e855f3d0c7
Dreakj <469982670@qq.com>
2022-10-09 19:42:01 +0800

    2022-8-27

--- guli-admin/src/api/sta.js
+++ guli-admin/src/api/sta.js
@@ -3,8 +3,14 @@ import request from '@/utils/request'
 export default {
     createStatistics(day) {
         return request({
-            url:'/staservice/sta/regosterCount/'+day,
+            url:'/staservice/sta/registerCount/'+day,
             method:'post',
+        })
+    },
+    showChart(searchObj) {
+        return request({
+            url:'/staservice/sta/showData/'+ searchObj.begin+'/'+searchObj.start+'/'+searchObj.end,
+            method:'get',
         })
     }
 }
